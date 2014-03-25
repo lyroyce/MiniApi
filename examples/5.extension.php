054743f4bafc4d0fd01132db8fc45e98cd0cb28e
lyroyce <lyroyce@sina.com>
2014-03-25 10:15:41 +0800

    Update documentation

--- examples/5.extension.php
+++ examples/5.extension.php
@@ -2,7 +2,13 @@
 require_once __DIR__.'/../core/MiniApi.php';
 $api = new MiniApi();
 
-echo "1. register a new protocol handler for SOAP protocol.\n";
+echo "1. Define a new protocol handler for SOAP protocol.\n";
+class MySoapClient extends MiniProtocol{
+	protected function init_auth_registration(){}
+	protected function send(MiniRequest $request, MiniResponse $response){}
+}
+
+echo "2. register and use the new protocol handler.\n";
 $api->register_protocol('SOAP', 'MySoapClient'); // this may cause an expection: Class 'MySoapClient' not found
 echo $api->request('http://www.google.com/?wsdl', 'User.New', 'SOAP')
 	->call('{"name":"John"}')->debug();
